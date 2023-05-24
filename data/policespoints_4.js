d51f6d3856a90a7d740311dde26fb201b21ca5d4
mauritiushigh <134492957+mauritiushigh@users.noreply.github.com>
2023-05-24 11:11:06 +0200

    Add files via upload

---
+++ data/policespoints_4.js
@@ -0,0 +1,10 @@
+var json_policespoints_4 = {
+"type": "FeatureCollection",
+"name": "policespoints_4",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "COMPNT_NM": "HARARE", "LOCATION_X": 18.6693, "LOCATION_Y": -34.05193, "CREATE_DT": "20221122", "VERSION": "1.2.7" }, "geometry": { "type": "Point", "coordinates": [ 18.66930000052076, -34.051929999887498 ] } },
+{ "type": "Feature", "properties": { "COMPNT_NM": "LINGELETHU-WEST", "LOCATION_X": 18.66004, "LOCATION_Y": -34.04298, "CREATE_DT": "20221122", "VERSION": "1.2.7" }, "geometry": { "type": "Point", "coordinates": [ 18.66004000091338, -34.042980000268514 ] } },
+{ "type": "Feature", "properties": { "COMPNT_NM": "KHAYELITSHA", "LOCATION_X": 18.66599, "LOCATION_Y": -34.02464, "CREATE_DT": "20221122", "VERSION": "1.2.7" }, "geometry": { "type": "Point", "coordinates": [ 18.665990000046854, -34.024639999812827 ] } }
+]
+}
